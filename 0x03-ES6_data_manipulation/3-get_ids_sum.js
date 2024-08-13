cb41f9dfb2c3383ec21befe23b54a53f89209ffb
abdallhfattah <abdallh.fattah33@gmail.com>
2024-08-13 14:50:09 +0300

    summing ids

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -1,5 +1,3 @@
-const getStudentIdsSum = (students) => {
-  students.reduce((id_sum, student) => id_sum + student.id, 0);
-}
+const getStudentIdsSum = (students) => students.reduce((id_sum, student) => id_sum + student.id, 0);
 
-export default getStudentIdsSum();
+export default getStudentIdsSum;
