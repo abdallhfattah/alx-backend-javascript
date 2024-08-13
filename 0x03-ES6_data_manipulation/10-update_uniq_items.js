d88a5eeb36ea33c9b9daa509110d4eb507dc758f
abdallhfattah <abdallh.fattah33@gmail.com>
2024-08-13 20:48:05 +0300

    unique

---
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -0,0 +1,13 @@
+const updateUniqueItems = (map) => {
+  if (!(map instanceof Map)) throw new Error("Cannot process");
+
+  map.forEach((quantity, name) => {
+    if (quantity === 1) {
+      map.delete(name);
+      map.set(name, 100);
+    }
+  });
+  return map;
+};
+
+export default updateUniqueItems;
