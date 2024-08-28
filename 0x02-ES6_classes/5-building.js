1d18ff09135146c70cd49bd1348aba4151546111
abdallhfattah <abdallh.fattah33@gmail.com>
2024-08-28 17:58:04 +0300

    building

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,6 +1,6 @@
 class Building {
   constructor(sqft) {
-    if (this.constuctor !== Building) {
+    if (this.constructor !== Building) {
       if (typeof this.evacuationWarningMessage !== 'function') {
         throw new Error('Class extending Building must override evacuationWarningMessage');
       }
