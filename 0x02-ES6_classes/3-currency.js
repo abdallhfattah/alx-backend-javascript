767c1bb443094a193e8df73eb3cf2f2effcc5dee
abdallhfattah <abdallh.fattah33@gmail.com>
2024-08-28 15:20:48 +0300

    currency

---
+++ 0x02-ES6_classes/3-currency.js
@@ -0,0 +1,33 @@
+class Currency {
+  constructor(code, name) {
+    this._code = code;
+    this._name = name;
+  }
+
+  set code(code) {
+    if (typeof code !== 'string') {
+      throw new TypeError('Code must be a string!');
+    }
+    this._code = code;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  set name(name) {
+    if (typeof name !== 'string') {
+      throw new TypeError('name must be a string!');
+    }
+    this._code = name;
+  }
+
+  get name() {
+    return this._code;
+  }
+
+  displayFullCurrency() {
+    return `${this._name} (${this._code})`;
+  }
+}
+export default Currency;
